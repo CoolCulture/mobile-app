45ec1fe630ff0683f9e6e8e68554e5eabc0d8548
Brandon Cook <bcook@thoughtworks.com>
2014-03-14 14:04:32 -0400

    Added ngAnimate to the coolCultureApp

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function(config) {
       'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-animate/angular-animate.js',
       // 'app/bower_components/angular-scenario/angular-scenario.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
