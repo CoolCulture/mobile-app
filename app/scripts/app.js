a32f1b77cf1366caf686410b7d9741a18afd8ef4
syntactical <greg.dutcher@gmail.com>
2014-03-17 16:31:41 -0400

    add radio buttons for filters

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('coolCultureApp', [
+  'ui.bootstrap',
   'ngAnimate',
   'ngCookies',
   'ngResource',
--- app/scripts/controllers/museumList.js
+++ app/scripts/controllers/museumList.js
@@ -1,11 +1,29 @@
 'use strict';
 
 angular.module('coolCultureApp')
-  .controller('MuseumListCtrl', function($scope, MuseumService) {
+  .controller('MuseumListCtrl', function($scope, $modal, MuseumService) {
     $scope.museums = MuseumService.requestAllMuseums();
     $scope.museumFilters = {
     	category: "",
     	borough: "",
     	subwayLine: ""
-    }
+    };
+
+    $scope.openFilterModal = function() {
+    	var modalInstance = $modal.open({
+    		templateUrl: 'filters.html',
+	    	controller: function ($scope, $modalInstance, museumFilters) {
+	    		$scope.museumFilters = museumFilters;
+	    		$scope.ok = function () {
+	    			$modalInstance.close();
+	    		};
+	    	},
+	    	resolve: {
+	    		museumFilters: function () {
+	    			return $scope.museumFilters;
+	    		}
+	    	}
+    	});
+    };
+    
   });
