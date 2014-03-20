c0848bfca94be28953b1026e4e9e9176fa7c9dfb
syntactical <greg.dutcher@gmail.com>
2014-03-20 13:06:14 -0400

    regenerate app without ActiveRecord

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MobileApp::Application.config.secret_key_base = '7a00a79e30fcdfc4c1c557ec070de2cd20fa5aacd110c3c6d6aea17ed4627514c4715c0d1437f9f721f69f9d3a7e285631e5d7c6d54ee5f709a624a88e50495d'
+MobileApp::Application.config.secret_key_base = 'b4b05fd14a0309e828758e4f7137b31aff64518db1237feb7b70fd42d850b388dd1724e13c6707dabf2218f59eee1e3d0043734bb9663bee44e2a66011e0a47a'
