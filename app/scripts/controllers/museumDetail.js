1210c2bfe7fd354e47a739541c933cc0c0af56ac
Brandon Cook <bcook@thoughtworks.com>
2014-03-20 11:55:28 -0400

    Committing deleted files that were moved in previous commit

--- app/scripts/controllers/museumDetail.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('coolCultureApp')
-  .controller('MuseumDetailCtrl', function ($scope, $routeParams, MuseumService) {
-    MuseumService.requestMuseum($routeParams.id).then(function(data){
-      $scope.museum = data;
-    });
-  });
--- app/scripts/filters/phonenumberfilter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('coolCultureApp')
-  .filter('phoneNumberFilter', function () {
-    return function (input) {
-      return input.split('-').join('').split('(').join('').split(')').join('');
-    };
-  });
