d1be70b8cafcba8c544eddf5a8c53479f267dda6
Brandon Cook <bcook@thoughtworks.com>
2014-03-20 14:20:03 -0400

    Changed clean to use the force options to clean the public directory because it is outside of the directory; needed to run grunt build when a public directory exists

--- angular/Gruntfile.js
+++ angular/Gruntfile.js
@@ -119,6 +119,9 @@ module.exports = function (grunt) {
     // Empties folders to start fresh
     clean: {
       dist: {
+        options: {
+          force: true
+        },
         files: [{
           dot: true,
           src: [
