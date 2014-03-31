a86c21a469311d6d1d70c4bf766be6bf6bcfbf3b
syntactical <greg.dutcher@gmail.com>
2014-03-31 14:37:24 -0400

    move MuseumController tests to spec folder

---
+++ spec/controllers/museums_controller_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe MuseumsController do
+  
+  describe "GET index" do
+    it "assigns @museums" do
+      museum = FactoryGirl.create(:museum)
+      get :index
+      expect(assigns(:museums)).to eq([museum])
+    end
+  end
+
+  describe "GET new" do
+    it "should get new" do
+      get :new
+      expect(response).to render_template("new")
+    end
+
+    it "should create new museum" do
+      get :new
+      response.should be_ok
+      assigns[:museum].should be_present
+    end
+  end
+
+  describe "POST create" do
+    subject{post :create, museum: FactoryGirl.attributes_for(:museum)}
+
+    it "should create museum" do
+      expect{subject}.to change(Museum, :count).by(1)
+    end
+
+    it "should redirect to show page for museum with create success message" do
+      subject.should redirect_to assigns(:museum)
+      flash[:notice].should eq("Museum was successfully created.")
+    end
+  end
+
+  describe "GET show" do
+    it "should return success" do
+      museum = FactoryGirl.create(:museum)
+      get :show, id: museum.id
+      response.should be_ok
+    end
+  end
+
+  describe "GET edit" do
+    it "should return success" do
+      museum = FactoryGirl.create(:museum)
+      get :edit, id: museum.id
+      response.should be_ok
+    end
+  end
+
+  describe "PATCH update" do    
+    it "should update museum" do
+      museum = FactoryGirl.create(:museum)
+      patch :update, id: museum.id, museum: FactoryGirl.attributes_for(:museum, name:"A New Museum Name")
+      museum.reload
+      museum.name.should eq("A New Museum Name")
+
+      response.should redirect_to assigns(:museum)
+    end
+  end
+
+  describe "DELETE" do
+    it "should destroy museum" do
+      museum = FactoryGirl.create(:museum)
+      expect{post :destroy, id: museum.id}.to change(Museum, :count).by(-1)
+    end
+  end
+end
