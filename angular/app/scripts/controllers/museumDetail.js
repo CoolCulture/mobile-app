5a3caea00c0287fe13217defcb5cbeda4d10e4db
Brandon Cook <bcook@thoughtworks.com>
2014-04-15 16:33:10 -0400

    Added livefyre commenting engine; Minor modifications to filter styling

--- angular/app/scripts/controllers/museumDetail.js
+++ angular/app/scripts/controllers/museumDetail.js
@@ -4,7 +4,24 @@ angular.module('coolCultureApp')
   .controller('MuseumDetailCtrl', function ($scope, $routeParams, $window, MuseumService) {
     MuseumService.requestMuseum($routeParams.id).success(function(data){
       $scope.museum = data;
+
+      (function () {
+      var articleId = fyre.conv.load.makeArticleId($scope.museum.name);
+      fyre.conv.load({}, [{
+          el: 'livefyre-comments',
+          network: "livefyre.com",
+          siteId: "357315",
+          articleId: articleId,
+          signed: false,
+          collectionMeta: {
+              articleId: articleId,
+              url: fyre.conv.load.makeCollectionUrl(),
+          }
+        }], function() {});
+      }());
     });
 
     $window.scrollTo(0,0);
+
+
   });
