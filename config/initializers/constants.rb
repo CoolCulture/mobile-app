1d3eb2671d1a2c12324c73fd24f2288b7f33dfbf
B. Wesley Reid <bwreid@gmail.com>
2014-10-27 19:19:59 -0600

    correct staging url in constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 BASE_URL =  if Rails.env.development?
               "http://localhost:3000/"
             elsif Rails.env.staging?
-              "http://cooler-culture.heroku.com/"
+              "http://cooler-culture.herokuapp.com/"
             elsif Rails.env.production?
               "http://coolculture.com/"
             end
