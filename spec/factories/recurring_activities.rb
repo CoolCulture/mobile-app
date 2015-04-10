55fb7f5cc4696c790c10a85da521173667617109
B. Wesley Reid <bwreid@gmail.com>
2015-04-10 12:13:34 -0600

    update Gemfile.lock

--- spec/factories/recurring_activities.rb
+++ spec/factories/recurring_activities.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :recurring_activity do
-    museum { FactoryGirl.create(:museum, name: Faker::Venue.name) }
+    museum { FactoryGirl.create(:museum, name: FFaker::Venue.name) }
     name 'Watch the stars'
     description 'Just what it says'
     schedule "{\"interval\":1,\"until\":null,\"count\":null,\"validations\":{\"day_of_week\":{},\"day_of_month\":[11]},\"rule_type\":\"IceCube::MonthlyRule\",\"week_start\":0}"
