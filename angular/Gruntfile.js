2bb519ecdf18c4680c8cab6f49ca20c54138bf32
B. Wesley Reid <bwreid@gmail.com>
2015-06-28 20:28:28 -0600

    wip

--- angular/Gruntfile.js
+++ angular/Gruntfile.js
@@ -12,6 +12,7 @@ module.exports = function (grunt) {
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
 
+  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-connect-proxy');
   grunt.loadNpmTasks('grunt-karma');
 
