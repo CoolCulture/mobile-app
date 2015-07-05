58c851af80d66f1b22d88983882a554d79008b1d
B. Wesley Reid <bwreid@gmail.com>
2015-07-05 11:08:56 -0600

    [WR] update staging mail settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -68,6 +68,8 @@ MobileApp::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Send emails through Mandrill
+  config.action_mailer.default_url_options = { :host => 'cooler-culture.herokuapp.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.mandrillapp.com',
